3eb84c8f0f04b584bf297842b7a194675b34008f
Aleksandr Gordeev <alexander.gordeev@vk.team>
2023-01-29 01:11:28 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@fonts': path.resolve(__dirname, './src/assets/fonts')
+        }
+    },
+    plugins: [
+        legacy({
+            targets: [
+                'defaults',
+                'not IE 11'
+            ]
+        }),
+        react({
+            exclude: /\.stories\.(t|j)sx?$/,
+            include: '**/*.tsx'
+        }),
+        svgr({})
+    ],
+    css: {
+        preprocessorOptions: {
+            styl: {
+                imports: [
+                    path.resolve(__dirname, 'src/styles/common.styl')
+                ],
+            },
+        },
+    },
+})
